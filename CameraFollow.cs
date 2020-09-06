4fd3f987f6681afed472a1ce2fb487346cfca99c
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2020-09-07 01:15:57 +0800

    Update CameraFollow.cs

--- CameraFollow.cs
+++ CameraFollow.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Camera cam;
-    public GameObject player;
-    public float CamMove = 0.05f;
-    // Update is called once per frame
+    public GameObject player; //Fetched in Start()
+    public float CamMove = 0.05f, CamRange = 1;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
     void Update()
     {
-        cam.transform.position = Vector3.MoveTowards(cam.transform.position, new Vector3(player.transform.position.x + (Input.GetAxis("Horizontal")), player.transform.position.y + (Input.GetAxis("Vertical")), -10f), CamMove);   
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x + (Input.GetAxis("Horizontal")/CamRange), player.transform.position.y + (Input.GetAxis("Vertical") / CamRange), -10f), CamMove);   
     }
 }
