01f1f2f5988f2e1d1292117f4bf6fde710e228d8
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2020-09-07 01:15:22 +0800

    Update AttackScript.cs

--- AttackScript.cs
+++ AttackScript.cs
@@ -1,27 +1,35 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AttackScript : MonoBehaviour
+public class EnemyAttack : MonoBehaviour
 {
-    public GameObject player;
-    public GameObject enemy;
-    public float AttackRange = 3;
-    public Mask SwordMask;
+    public GameObject player; //Fetched in Start()
+    public GameObject cam; //Fetched in Start()
 
-    public float DamageMin = 5f, DamageMax = 10f;
+    public float AttackRange = 3;
+    public float DamageMin = 5f, DamageMax = 10f, Damage;
     public int AttackCooldown = 500, AttackTime = 0;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         if (AttackTime == 0)
         {
-            if (Vector2.Distance(player.transform.position, enemy.transform.position) < AttackRange)
+            if (Vector2.Distance(player.transform.position, transform.position) < AttackRange)
             {
-                PlayerMovement.MinusHealth(Random.Range(DamageMin, DamageMax));
+                Damage = Random.Range(DamageMin, DamageMax);
+                player.GetComponent<PlayerGeneral>().MinusHealth(Damage);
+                cam.GetComponent<CameraGeneral>().CamShake(Damage/100,30);
                 AttackTime = AttackCooldown;
             }
         }
