eee5d44fc96c4167e17516ecfd674c904f977b95
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2020-09-08 01:44:30 +0800

    Add files via upload

---
+++ LootGeneral.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootGeneral : MonoBehaviour
+{
+    public bool IsFollowing;
+    public float AttentionRange = 1f, PickupRange = 0.2f ,PickupSpeed = 0.5f;
+    public GameObject Player;
+    public int WeaponID;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        float DistToPlayer = Vector2.Distance(transform.position, Player.transform.position);
+        if (IsFollowing)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Player.transform.position,PickupSpeed);
+        }
+        if (DistToPlayer <= AttentionRange)
+        {
+            IsFollowing = true;
+        }
+        if (DistToPlayer <= PickupRange)
+        {
+            Debug.Log(WeaponID);
+            Player.GetComponent<PlayerGeneral>().AppendInventory(1,WeaponID);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ LootSpawning.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LootSpawning : MonoBehaviour
+{
+    public Sprite[] WeaponSpriteList = { };
+    public GameObject WeaponLoot;
+    public SpriteRenderer Renderer;
+
+    public class Weapon
+    {
+        public string WeaponName;
+        public float DamageMin, DamageMax;
+        public Weapon(string name,float DmgRangeMin, float DmgRangeMax)
+        {
+            WeaponName = name;
+            DamageMin = DmgRangeMin;
+            DamageMax = DmgRangeMax;
+        }
+    }
+
+    public Weapon[] WeaponList;
+
+    void Awake()
+    {
+        Renderer = WeaponLoot.AddComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        WeaponList.Append(new Weapon("Copper ShortSword", 10f, 10));
+        WeaponList.Append(new Weapon("Silver Shortsword", 15f, 15f));
+        WeaponList.Append(new Weapon("Gold Shortsword", 20f, 20f));
+    }
+
+    void Update() {}
+
+    public void SpawnWeaponLoot(float XPos,float YPos,int Weapon_Id)
+    {
+        WeaponLoot.GetComponent<SpriteRenderer>().sprite = WeaponSpriteList[Weapon_Id];
+        WeaponLoot.GetComponent<LootGeneral>().WeaponID = Weapon_Id;
+
+        Instantiate(WeaponLoot,new Vector2(XPos,YPos),Quaternion.identity);
+    }
+}
