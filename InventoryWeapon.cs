c56cea7ca5e6d5f86c8b598a783f702427f9be3d
asian_nerd <ironryan2807@gmail.com>
2020-09-24 05:21:39 +0800

    Blits Weapon name onto canvas
    Blits weapons name onto canvas but atm changes all `Button` objects text to the corresponding index when fetched with `FindObjectsWithType<>`
    
    - The other two scripts are for blitting the players health and weapons name

---
+++ InventoryWeapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryWeapon : MonoBehaviour
+{
+    public Button[] ButtonList;
+    void Awake()
+    {
+        ButtonList = FindObjectsOfType<Button>();
+    }
+
+    void Update()
+    {
+        ButtonList = FindObjectsOfType<Button>();
+        Debug.Log(ButtonList.Length);
+        Debug.Log(ButtonList);
+        for (int i = 0;i<ButtonList.Length;i++)
+        {
+            Debug.Log(i);
+            Debug.Log(ButtonList[i].GetComponentInChildren<Text>().text);
+            ButtonList[i].GetComponentInChildren<Text>().text = i.ToString();
+        }
+    }
+}
---
+++ TextWeaponScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWeaponScript : MonoBehaviour
+{
+    public Text SelfText;
+    public GameObject Parent;
+    public Camera MainCam;
+    public GameObject WeaponLootParent;
+    void Start() 
+    {
+        MainCam = FindObjectOfType<Camera>();
+    }
+
+    void Update()
+    {
+        Debug.Log(SelfText.text);
+        Debug.Log(Parent.GetComponent<LootGeneral>().WeaponName);
+        SelfText.text = Parent.GetComponent<LootGeneral>().WeaponName;
+        transform.position = MainCam.WorldToScreenPoint(WeaponLootParent.transform.position);
+    }
+}
