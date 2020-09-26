f3dd5df3a9799007f334575c720b6a60e4ea53b7
asian_nerd <ironryan2807@gmail.com>
2020-09-27 03:39:39 +0800

    Renders the current weapon name in the inventory menu

---
+++ PlayerEquippedWeaponText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerEquippedWeaponText : MonoBehaviour
+{
+    public GameObject Player;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        GetComponentInChildren<Text>().text = Player.GetComponent<PlayerGeneral>().CurrentWeapon.WeaponName;
+    }
+}
