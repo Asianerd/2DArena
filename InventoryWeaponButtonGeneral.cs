2a9c4c8933a6af2016ac484484712dfc2f167dca
asian_nerd <ironryan2807@gmail.com>
2020-09-29 01:16:52 +0800

    Cant sleep so i did this

---
+++ InventoryWeaponButtonGeneral.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryWeaponButtonGeneral : MonoBehaviour
+{
+    public GameObject Player;
+
+
+    public void SetEquippedWeapon()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Player.GetComponent<PlayerGeneral>().CurrentWeapon = new PlayerGeneral.Weapon("New Weapon",1f,10f,10f,10f,100);
+    }
+}
