af4e9ea5e929396196407ae6c6614743b810c47f
asian_nerd <ironryan2807@gmail.com>
2020-10-05 04:09:17 +0800

    Created Melee,Range,Projectile overloads

--- WeaponData.cs
+++ WeaponData.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq.Expressions;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponData : MonoBehaviour
 {
+    public List<GameObject> RangeProjectilePrefabList;
+    public List<GameObject> ProjectilePrefabList;
+
+    public GameObject GenericRangeProjectile;
+    public GameObject GenericProjectile;
+
+
     public class Weapon
     {
         public string WeaponName;
-        public float DamageMin, DamageMax, WeaponRange, Knockback;
-        public int WeaponCooldown,Rarity,Durability,MaxDurability;
+        public float DamageMin, DamageMax, Knockback;
+        public int WeaponCooldown, Rarity, Durability, MaxDurability;
         public bool IsBreakable;
+        public int ManaUsage;
         public int Category;
         public int[][] CategoryReference = new int[][] { new int[] { 0, 1, 2, 3, 4 }, new int[] { 0, 1 }, new int[] { 0, 1, 2, 3 } };
         /* Category
@@ -67,41 +73,110 @@ public class WeaponData : MonoBehaviour
          * (4) Slow
          * 
          */
-        public Weapon(string name, float DmgRangeMin, float DmgRangeMax, float WpnKnockback, float WpnRange, int WpnCooldown, int WpnRarity, int WpnMaxDurability,int WpnCategory,int WpnType,int WpnEffect = 0)
+        // Data for each Category
+        // Melee
+        public float AttackRange = 0;
+        // Range
+        public GameObject RangeProjectile;
+        public int ProjectileShelfLife;
+        public float SpawnedProjectileSpeed;
+        // Projectile
+        public GameObject Projectile;
+        public int ShelfLife;
+        public int Used;
+        public int Amount;
+        public float ProjectileSpeed;
+        /* Overload order
+         * 
+         * Melee
+         * Range
+         * Projectile
+         */
+        // Melee
+        public Weapon(string name, float DmgRangeMin, float DmgRangeMax, float WpnKnockback, int WpnCooldown, int WpnRarity, int WpnCategory, int WpnType, 
+            float WpnRange
+            , int WpnMaxDurability = -10, int WpnEffect = 0, int WpnMana= 0 )
         {
+            // Default values for every weapon
             WeaponName = name;
             DamageMin = DmgRangeMin;
             DamageMax = DmgRangeMax;
             Knockback = WpnKnockback;
-            WeaponRange = WpnRange;
             WeaponCooldown = WpnCooldown;
+
+            if (WpnMaxDurability == -10)
+                IsBreakable = false;
             MaxDurability = WpnMaxDurability;
             Durability = MaxDurability;
-            Rarity = WpnRarity;
 
+            Rarity = WpnRarity;
             Category = WpnCategory;
             Type = WpnType;
-
             Effect = WpnEffect;
+            ManaUsage = WpnMana;
+
+            // Melee
+            AttackRange = WpnRange;
         }
-        public Weapon(string name, float DmgRangeMin, float DmgRangeMax, float WpnKnockback, float WpnRange, int WpnCooldown, int WpnRarity, bool WpnIsBreakable, int WpnCategory, int WpnType, int WpnEffect=0)
+
+        // Range
+        public Weapon(string name, float DmgRangeMin, float DmgRangeMax, float WpnKnockback, int WpnCooldown, int WpnRarity, int WpnCategory,int WpnType, 
+            int WpnShelfLife, GameObject WpnProjectile, float WpnSpwnProjectileSpeed
+            , int WpnMaxDurability = -10, int WpnEffect = 0,int WpnMana=0)
         {
+            // Default values for every weapon
             WeaponName = name;
             DamageMin = DmgRangeMin;
             DamageMax = DmgRangeMax;
             Knockback = WpnKnockback;
-            WeaponRange = WpnRange;
             WeaponCooldown = WpnCooldown;
-            MaxDurability = 1;
-            Durability = MaxDurability;
-            Rarity = WpnRarity;
 
+            if (WpnMaxDurability == -10)
+                IsBreakable = false;
+            MaxDurability = WpnMaxDurability;
+            Durability = MaxDurability;
+
+            Rarity = WpnRarity;
             Category = WpnCategory;
             Type = WpnType;
-
             Effect = WpnEffect;
+            ManaUsage = WpnMana;
 
-            IsBreakable = WpnIsBreakable;
+            // Range
+            RangeProjectile = WpnProjectile;
+            ProjectileShelfLife = WpnShelfLife;
+            SpawnedProjectileSpeed = WpnSpwnProjectileSpeed;
+        }
+
+        // Projectile
+        public Weapon(string name, float DmgRangeMin, float DmgRangeMax, float WpnKnockback, int WpnCooldown, int WpnRarity, int WpnCategory, int WpnType,
+            int WpnShelfLife, GameObject WpnProjectile, int WpnAmount, float WpnProjectileSpeed
+            , int WpnMaxDurability = -10, int WpnEffect = 0,int WpnMana=0)
+        {
+            // Default values for every weapon
+            WeaponName = name;
+            DamageMin = DmgRangeMin;
+            DamageMax = DmgRangeMax;
+            Knockback = WpnKnockback;
+            WeaponCooldown = WpnCooldown;
+
+            if (WpnMaxDurability == -10)
+                IsBreakable = false;
+            MaxDurability = WpnMaxDurability;
+            Durability = MaxDurability;
+
+            Rarity = WpnRarity;
+            Category = WpnCategory;
+            Type = WpnType;
+            Effect = WpnEffect;
+            ManaUsage = WpnMana;
+
+            // Projectile
+            Projectile = WpnProjectile;
+            ShelfLife = WpnShelfLife;
+            Amount = WpnAmount;
+            Used = 0;
+            ProjectileSpeed = WpnProjectileSpeed;
         }
     }
 
@@ -109,17 +184,29 @@ public class WeaponData : MonoBehaviour
 
     void Awake()
     {
-        void Addw(string Name, float dmgmin, float dmgmax, float wpnknock, float wpnrange, int cooldown, int rarity, int category, int type, int maxdurability = -10,int fx = 0)
-        {
+        /*void Addw(string Name, float dmgmin, float dmgmax, float wpnknock, float wpnrange, int cooldown, int rarity, int category, int type, int maxdurability = -10,int fx = 0)
             if (maxdurability == -10) GlobalWeaponList.Add(new Weapon(Name, dmgmin, dmgmax, wpnknock, wpnrange, cooldown, rarity, false, category, type, fx));
-            else GlobalWeaponList.Add(new Weapon(Name, dmgmin, dmgmax, wpnknock, wpnrange, cooldown, rarity, maxdurability, category, type, fx));
+            *else GlobalWeaponList.Add(new Weapon(Name, dmgmin, dmgmax, wpnknock, wpnrange, cooldown, rarity, maxdurability, category, type, fx));
+        Addw("Aluminium shortsword", 3f, 4f, 1.5f, 1.5f, 100, 0, 0, 0, 100);*/
+        void m(string Name, float dmgmin, float dmgmax, float wpnknock, int cooldown, int rarity, int category, int type, float range,int maxdurability = -10, int fx = 0,int mana=0)
+        {
+            GlobalWeaponList.Add(new Weapon(Name, dmgmin, dmgmax, wpnknock, cooldown, rarity, category, type, range, maxdurability, fx,mana));
         }
-        Addw("Aluminium shortsword", 3f, 4f, 1.5f, 1.5f, 100, 0, 0, 0, 100);
+        void r(string Name, float dmgmin, float dmgmax, float wpnknock, int cooldown, int rarity, int category, int type, int shelflife, GameObject projectile,float speed, int maxdurability = -10, int fx =0,int mana=0)
+        {
+            GlobalWeaponList.Add(new Weapon(Name, dmgmin, dmgmax, wpnknock, cooldown, rarity, category, type, shelflife, projectile, speed,maxdurability, fx,mana));
+        }
+        void p(string Name, float dmgmin, float dmgmax, float wpnknock, int cooldown, int rarity, int category, int type, int shelflife, GameObject projectile,float speed, int amount,int maxdurability = -10, int fx = 0,int mana=0)
+        {
+            GlobalWeaponList.Add(new Weapon(Name, dmgmin, dmgmax, wpnknock, cooldown, rarity, category, type, shelflife,projectile,amount,speed,maxdurability, fx,mana));
+        }
+        m("Aluminium shortsword", 3, 4, 3f, 100, 0, 0, 0, 1.5f, 60);
+        m("Silicon shortsword", 3, 4, 2f, 100, 0, 0, 0, 10);
+        m("Iron shortsword", 5, 6, 2, 100, 0, 0, 0, 5);
+
+        r("Bow", 5, 10, 1, 1, 1, 1, 0, 500, GenericRangeProjectile, 1);
+        Debug.Log(GlobalWeaponList[3].WeaponName);
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
