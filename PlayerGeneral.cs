23766aa550043e82d89e0d465e4e900512d21738
asian_nerd <ironryan2807@gmail.com>
2020-10-05 04:09:40 +0800

    Attack now depends on CurrentWeapon.Category

--- PlayerGeneral.cs
+++ PlayerGeneral.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Transactions;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UIElements;
 
 public class PlayerGeneral : MonoBehaviour
 {
@@ -11,7 +14,9 @@ public class PlayerGeneral : MonoBehaviour
     public GameObject RuntimeScript;
     public GameObject DamageBubblePrefab;
 
-    
+
+
+
 
     //HP
     public float HP = 100, HPMax = 100, HPRegen = 1;
@@ -20,16 +25,21 @@ public class PlayerGeneral : MonoBehaviour
     public List<ArrayList> InventoryLoot = new List<ArrayList>();
     public List<string> InventoryLootName = new List<string>();
     public WeaponData.Weapon EquippedWeapon;
-    public List<WeaponData.Weapon> GlobalWeaponList = new List<WeaponData.Weapon>();
 
     // ATK
     public GameObject[] EnemyArray;
-    public float AttackRange = 1f;
     public float PlayerDamage = 3f;
-    public WeaponData.Weapon CurrentWeapon = new WeaponData.Weapon("Fists", 2, 3, 5, 2, 50,0,false,0,0);
+
+    public WeaponData.Weapon CurrentWeapon = new WeaponData.Weapon("Fists", 50, 100, 1, 10, 0, 0, 0, 5);
+
+    public int WeaponCooldown = 0;
 
     void Update()
     {
+
+        //double angle = Math.Atan2()
+
+
         if (!RuntimeScript.GetComponent<InventoryShow>().GamePaused)
         {
             Regen();
@@ -40,24 +50,74 @@ public class PlayerGeneral : MonoBehaviour
     {
         HP -= losthealth;
     }
-    
+
     void Attack()
     {
-        float DamageInflicted = Random.Range(CurrentWeapon.DamageMin, CurrentWeapon.DamageMax);
+        if ((WeaponCooldown == 0) && Input.GetMouseButtonDown(0))
+        {
+            WeaponCooldown = CurrentWeapon.WeaponCooldown;
+            switch (CurrentWeapon.Category)
+            {
+                case 0:
+                    MeleeAttack();
+                    break;
+                case 1:
+                    RangeAttack();
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;
+            }
+
+            /*
+            EnemyArray = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject i in EnemyArray)
+            {
+                Vector3 EnemyPosition = new Vector3(i.transform.position.x, i.transform.position.y);
+                if ((i.GetComponent<EnemyGeneral>().DistanceEnemy(transform.position.x, transform.position.y) < AttackRange) && (Input.GetMouseButtonDown(0)))
+                {
+                    i.GetComponent<EnemyGeneral>().MinusHealth(PlayerDamage + DamageInflicted, CurrentWeapon.Knockback, transform.position);
+                    Instantiate(DamageBubblePrefab, EnemyPosition, Quaternion.identity).GetComponent<FXDamageBubbleGeneral>().Damage = DamageInflicted;
+                    WeaponCooldown = CurrentWeapon.WeaponCooldown;
+                }
+            }*/
+        }
+        if (WeaponCooldown > 0) WeaponCooldown--;
+    }
+
+    void MeleeAttack()
+    {
+        float DamageInflicted = UnityEngine.Random.Range(CurrentWeapon.DamageMin, CurrentWeapon.DamageMax);
         EnemyArray = GameObject.FindGameObjectsWithTag("Enemy");
         foreach (GameObject i in EnemyArray)
         {
-            Vector3 EnemyPosition = new Vector3(i.transform.position.x, i.transform.position.y);
-            if ((i.GetComponent<EnemyGeneral>().DistanceEnemy(transform.position.x, transform.position.y) < AttackRange) && (Input.GetMouseButtonDown(0)))
+            Vector2 EnemyPosition = new Vector2(i.transform.position.x, i.transform.position.y);
+            if (i.GetComponent<EnemyGeneral>().DistanceEnemy(transform.position.x, transform.position.y) <= CurrentWeapon.AttackRange)
             {
-                DamageBubblePrefab.GetComponent<FXDamageBubbleGeneral>().Damage = DamageInflicted;
-                i.GetComponent<EnemyGeneral>().MinusHealth(PlayerDamage+DamageInflicted,CurrentWeapon.Knockback,transform.position);
-                Instantiate(DamageBubblePrefab, EnemyPosition,Quaternion.identity);
+                i.GetComponent<EnemyGeneral>().MinusHealth(PlayerDamage + DamageInflicted, CurrentWeapon.Knockback, transform.position);
+
+                GameObject obj = Instantiate(DamageBubblePrefab, EnemyPosition, Quaternion.identity);
+                obj.GetComponent<FXDamageBubbleGeneral>().Damage = DamageInflicted;
+                WeaponCooldown = CurrentWeapon.WeaponCooldown;
             }
         }
-        
     }
-    public void AppendInventory(int WeaponType, WeaponData.Weapon WantedWeapon)
+    void RangeAttack()
+    {
+        GameObject obj = Instantiate(CurrentWeapon.RangeProjectile, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+        //Debug.Log($"{Convert.ToSingle(Math.Atan2(Input.mousePosition.y-transform.position.y, Input.mousePosition.x-transform.position.x ))}   ::   {(Convert.ToSingle(Math.Atan2(Input.mousePosition.y-transform.position.y, Input.mousePosition.x-transform.position.x)))}");
+        double angle = Math.Atan2(Input.mousePosition.y-transform.position.y,Input.mousePosition.x-transform.position.x);
+
+        Debug.Log(angle);
+        obj.GetComponent<GenericRangeProjectile>().Set(angle, CurrentWeapon.SpawnedProjectileSpeed, CurrentWeapon.ProjectileShelfLife);
+    }
+    void ProjectileAttack()
+    {
+        Instantiate(CurrentWeapon.Projectile);
+    }
+
+    public void AppendInventory(WeaponData.Weapon WantedWeapon)
     {
         InvCanvas.GetComponent<InventoryWeapon>().AddWeapon(WantedWeapon);
         InventoryWeapon.Add(WantedWeapon);
