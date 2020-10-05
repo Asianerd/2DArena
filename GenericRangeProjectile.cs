81ab0496d0584f3087c40a1e1cb21b6dde68e715
asian_nerd <ironryan2807@gmail.com>
2020-10-06 03:47:30 +0800

    Added Damage functionality to collided objects

--- GenericRangeProjectile.cs
+++ GenericRangeProjectile.cs
@@ -1,22 +1,36 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class GenericRangeProjectile : MonoBehaviour
 {
+    public WeaponData.Weapon ProjectileWeapon;
     public double DirectionAngle;
     public float Speed;
     public int ShelfLife;
-    public double IncrementX, IncrementY;
+    public double TargetX,TargetY;
 
-    public void Set(double angle,float speed,int shelflife)
+    public void Set(double angle,float speed,int shelflife,WeaponData.Weapon weapon)
     {
         DirectionAngle = angle;
         Speed = speed;
         ShelfLife = shelflife;
-        IncrementX = Math.Cos(DirectionAngle) * Speed;
-        IncrementY = Math.Sin(DirectionAngle) * Speed;
+        TargetX = Math.Cos(DirectionAngle) * 100;
+        TargetY = Math.Sin(DirectionAngle) * 100;
+        ProjectileWeapon = weapon;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        double DamageInflicted = UnityEngine.Random.Range(ProjectileWeapon.DamageMin, ProjectileWeapon.DamageMax);
+        GameObject CollidedObject = collision.gameObject;
+        if(CollidedObject.CompareTag("Enemy"))
+        {
+            CollidedObject.GetComponent<EnemyGeneral>().MinusHealth(DamageInflicted,ProjectileWeapon.Knockback,transform.position);
+            Destroy(gameObject);
+        }
     }
 
     void Update()
@@ -24,7 +38,7 @@ public class GenericRangeProjectile : MonoBehaviour
         if (ShelfLife > 0)
         {
             ShelfLife--;
-            transform.position = new Vector2(transform.position.x+Convert.ToSingle(IncrementX), transform.position.y+Convert.ToSingle(IncrementY));
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(Convert.ToSingle(TargetX),Convert.ToSingle(TargetY)),Speed/10);
         }
         else
         {
