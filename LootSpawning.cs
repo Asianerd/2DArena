af8c260d1556167d2132db0bff9f8e4c1cc47df6
asian_nerd <ironryan2807@gmail.com>
2020-10-08 04:12:12 +0800

    Moved data initialization to `WeaponData`

--- LootSpawning.cs
+++ LootSpawning.cs
@@ -6,17 +6,16 @@ using UnityEngine.UI;
 
 public class LootSpawning : MonoBehaviour
 {
-    public Sprite[] WeaponSpriteList = { };
     public GameObject WeaponLoot;
     public SpriteRenderer Renderer;
+    public GameObject Runtime;
 
     public List<WeaponData.Weapon> WeaponList = new List<WeaponData.Weapon>();
-    public string[] WeaponNameArray = { "Copper shortsword", "Tin shortsword", "Iron shortsword", "Spear", "Amethyst staff", "Topaz staff", "Sapphire staff", "Amber staff", "Lead shortsword", "Silver shortsword", "Tungsten shortsword", "Iron broadsword", "Lead broadsword", "Silver broadsword", "Tungsten broadsword", "Emerald staff", "Ruby staff", "Crimson shortsword", "Gold shortsword", "Crimson broadsword", "Gold broadsword", "Trident", "Glaive", "Platinum shortsword", "Katana", "Platinum broadsword", "Diamond staff", "Last prism", "Star Wrath", "Phantasm", "Meowmere", "Celebration MK.2", "Solar Eruption", "Holy Water", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test" };
+    //public string[] WeaponNameArray = { "Copper shortsword", "Tin shortsword", "Iron shortsword", "Spear", "Amethyst staff", "Topaz staff", "Sapphire staff", "Amber staff", "Lead shortsword", "Silver shortsword", "Tungsten shortsword", "Iron broadsword", "Lead broadsword", "Silver broadsword", "Tungsten broadsword", "Emerald staff", "Ruby staff", "Crimson shortsword", "Gold shortsword", "Crimson broadsword", "Gold broadsword", "Trident", "Glaive", "Platinum shortsword", "Katana", "Platinum broadsword", "Diamond staff", "Last prism", "Star Wrath", "Phantasm", "Meowmere", "Celebration MK.2", "Solar Eruption", "Holy Water", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test" };
 
-    public void SpawnWeaponLoot(float XPos,float YPos, WeaponData.Weapon SelectedWeapon)
+    public void SpawnWeaponLoot(Vector3 SelectedVector3, WeaponData.Weapon SelectedWeapon)
     {
-        GameObject obj = Instantiate(WeaponLoot,new Vector2(XPos,YPos),Quaternion.identity);
-        obj.GetComponent<LootGeneral>().SetWeapon(SelectedWeapon);
-        obj.GetComponent<SpriteRenderer>().sprite = WeaponSpriteList[SelectedWeapon.WeaponID];
+        GameObject obj = Instantiate(WeaponLoot,SelectedVector3,Quaternion.identity);
+        obj.GetComponent<LootGeneral>().SetWeapon(SelectedWeapon,Runtime.GetComponent<WeaponData>().WeaponSpriteList[SelectedWeapon.WeaponID]);
     }
 }
