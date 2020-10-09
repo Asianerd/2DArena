6c399a3354638a9311bfb0ddc7cd501bd85bb88c
asian_nerd <ironryan2807@gmail.com>
2020-10-10 05:46:12 +0800

    Made 2 states; Cosmetic and Damage

--- WeaponObject.cs
+++ WeaponObject.cs
@@ -6,21 +6,28 @@ using UnityEngine;
 public class WeaponObject : MonoBehaviour
 {
     public GameObject Player;
-    public WeaponData.Weapon Weapon;
     float TotalAngle = 130;
     int CurrentCooldown = 0;
     float IncrementAngle;
+    float OffsetX, OffsetY;
+    public static bool IsSwinging;
+
+    public List<GameObject> EnemyAttackedList = new List<GameObject>();
 
 
     void Update()
     {
-        if (!InventoryShow.GamePaused)
+        if (!InventoryShow.GamePaused && IsSwinging)
         {
             transform.rotation = Quaternion.Euler(0f, 0f, transform.rotation.eulerAngles.z - IncrementAngle);
             CurrentCooldown++;
-            if (CurrentCooldown >= Weapon.WeaponCooldown) Destroy(gameObject);
+            if (CurrentCooldown >= Player.GetComponent<PlayerGeneral>().CurrentWeapon.WeaponCooldown)
+            {
+                IsSwinging = false;
+                EnemyAttackedList = new List<GameObject>();
+            }
 
-            transform.position = new Vector2(PlayerGeneral.PlayerPosition.x + 0.5f, PlayerGeneral.PlayerPosition.y + 0.5f);
+            transform.position = new Vector2(PlayerGeneral.PlayerPosition.x + OffsetX, PlayerGeneral.PlayerPosition.y + OffsetY);
         }
     }
 
@@ -29,24 +36,36 @@ public class WeaponObject : MonoBehaviour
         Player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    private void OnTriggerEnter2D(Collider2D Collision)
+
+
+    void OnTriggerStay2D(Collider2D Collision)
     {
-        if(Collision.CompareTag("Enemy") && !InventoryShow.GamePaused)
+        if(Collision.CompareTag("Enemy") && !InventoryShow.GamePaused && IsSwinging)
         {
-            double DamageInflicted = UnityEngine.Random.Range(Weapon.DamageMin, Weapon.DamageMax);
-            Collision.GetComponent<EnemyGeneral>().MinusHealth(DamageInflicted,Weapon.Knockback,PlayerGeneral.PlayerPosition);
+            if (!EnemyAttackedList.Contains(Collision.gameObject))
+            {
+                double DamageInflicted = UnityEngine.Random.Range(PlayerGeneral.CurrentWeaponReference.DamageMin, PlayerGeneral.CurrentWeaponReference.DamageMax);
+                Collision.GetComponent<EnemyGeneral>().MinusHealth(DamageInflicted, PlayerGeneral.CurrentWeaponReference.Knockback, PlayerGeneral.PlayerPosition);
+                EnemyAttackedList.Add(Collision.gameObject);
+            }
         }
     }
 
-    public void Set(WeaponData.Weapon weapon)
+    public void Swing()
     {
-        transform.position = new Vector2(PlayerGeneral.PlayerPosition.x + 0.5f, PlayerGeneral.PlayerPosition.y + 0.5f);
+        OffsetX = Mathf.Cos(PlayerGeneral.MouseAngle)*0.5f;
+        OffsetY = Mathf.Sin(PlayerGeneral.MouseAngle)*0.5f;
+
+        //Look towards (2D version)
+        transform.position = new Vector2(PlayerGeneral.PlayerPosition.x + OffsetX , PlayerGeneral.PlayerPosition.y + OffsetX);
         Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         diff.Normalize();
         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rot_z-90);
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+        //
 
-        Weapon = weapon;
-        IncrementAngle = TotalAngle / Weapon.WeaponCooldown; // Change this ; Speed(TotalAngle) is determined by the knockback
+        IncrementAngle = TotalAngle / PlayerGeneral.CurrentWeaponReference.WeaponCooldown; // Change this ; Speed(TotalAngle) is determined by the knockback
+        IsSwinging = true;
+        CurrentCooldown = 0;
     }
 }
