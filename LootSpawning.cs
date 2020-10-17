3941d2577c1b4cbd9a5a4373b79ff8d593cf068a
asian_nerd <ironryan2807@gmail.com>
2020-10-17 22:29:16 +0800

    Added correct sprites for `WeaponLoot`

--- LootSpawning.cs
+++ LootSpawning.cs
@@ -11,11 +11,21 @@ public class LootSpawning : MonoBehaviour
     public GameObject Runtime;
 
     public List<WeaponData.Weapon> WeaponList = new List<WeaponData.Weapon>();
-    //public string[] WeaponNameArray = { "Copper shortsword", "Tin shortsword", "Iron shortsword", "Spear", "Amethyst staff", "Topaz staff", "Sapphire staff", "Amber staff", "Lead shortsword", "Silver shortsword", "Tungsten shortsword", "Iron broadsword", "Lead broadsword", "Silver broadsword", "Tungsten broadsword", "Emerald staff", "Ruby staff", "Crimson shortsword", "Gold shortsword", "Crimson broadsword", "Gold broadsword", "Trident", "Glaive", "Platinum shortsword", "Katana", "Platinum broadsword", "Diamond staff", "Last prism", "Star Wrath", "Phantasm", "Meowmere", "Celebration MK.2", "Solar Eruption", "Holy Water", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test" };
 
     public void SpawnWeaponLoot(Vector3 SelectedVector3, WeaponData.Weapon SelectedWeapon)
     {
         GameObject obj = Instantiate(WeaponLoot, SelectedVector3, Quaternion.identity);
-        obj.GetComponent<LootGeneral>().SetWeapon(SelectedWeapon,Runtime.GetComponent<WeaponData>().MeleeWeaponSpriteList[SelectedWeapon.WeaponID]);
+        switch (SelectedWeapon.Category)
+        {
+            case 1:
+                obj.GetComponent<LootGeneral>().SetWeapon(SelectedWeapon, Runtime.GetComponent<WeaponData>().RangeWeaponSpriteList[SelectedWeapon.WeaponID]);
+                break;
+            case 2:
+                obj.GetComponent<LootGeneral>().SetWeapon(SelectedWeapon, Runtime.GetComponent<WeaponData>().ProjectileSpriteList[SelectedWeapon.WeaponID]);
+                break;
+            default:
+                obj.GetComponent<LootGeneral>().SetWeapon(SelectedWeapon, Runtime.GetComponent<WeaponData>().MeleeWeaponSpriteList[SelectedWeapon.WeaponID]);
+                break;
+        }
     }
 }
