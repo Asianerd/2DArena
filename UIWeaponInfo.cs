56b1e1f064f8088e3f7991d17aa8521cd01ad45e
asian_nerd <ironryan2807@gmail.com>
2020-10-17 22:33:32 +0800

    Bottom right text shows durability/projectiles used

---
+++ UIWeaponInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIWeaponInfo : MonoBehaviour
+{
+    public string DurabilityText;
+    int temp;
+
+    void Update()
+    {
+        if (temp > 10)
+        {
+            switch (PlayerGeneral.CurrentWeaponReference.Category)
+            {
+                case 2:
+                    DurabilityText = $"{PlayerGeneral.CurrentWeaponReference.Used}/{PlayerGeneral.CurrentWeaponReference.Amount}";
+                    break;
+                default:
+                    if (PlayerGeneral.CurrentWeaponReference.MaxDurability != -100)
+                        DurabilityText = $"{PlayerGeneral.CurrentWeaponReference.Durability}/{PlayerGeneral.CurrentWeaponReference.MaxDurability}";
+                    else
+                    {
+                        DurabilityText = "";
+                    }
+                    break;
+            }
+            GetComponentInChildren<Text>().text = DurabilityText;
+        }
+        else temp++;
+    }
+}
