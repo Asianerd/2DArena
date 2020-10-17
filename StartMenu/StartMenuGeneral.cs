bf363d91ff9d44f449aa7c49d77103b544bf6f85
asian_nerd <ironryan2807@gmail.com>
2020-10-17 22:32:29 +0800

    Created StartMenu.cs

---
+++ StartMenu/StartMenuGeneral.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenuGeneral : MonoBehaviour
+{
+    public GameObject Credits;
+    public GameObject Help;
+    public GameObject Start;
+
+    public void Awake()
+    {
+        Credits = GameObject.Find("CreditScreen");
+        Help = GameObject.Find("HelpScreen");
+        Start = GameObject.Find("StartScreen");
+        Credits.SetActive(false);
+        Help.SetActive(false);
+        Start.SetActive(true);
+    }
+
+    public void Normal()
+    {
+        Credits.SetActive(false);
+        Help.SetActive(false);
+        Start.SetActive(true);
+    }
+
+    public void OpenHelp()
+    {
+        Start.SetActive(false);
+        Help.SetActive(true);
+    }
+
+    public void OpenCredits()
+    {
+        Start.SetActive(false);
+        Credits.SetActive(true);
+    }
+}
