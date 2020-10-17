5b0af39e5916610336968a5140d6762cb9a4c91c
asian_nerd <ironryan2807@gmail.com>
2020-10-17 22:26:59 +0800

    Added delay so that errors arent risen at the first fram

--- CursorSpriteScript.cs
+++ CursorSpriteScript.cs
@@ -6,26 +6,32 @@ public class CursorSpriteScript : MonoBehaviour
 {
     public List<Sprite> CursorSpriteList;
     Camera MainCamera;
+    int temp;
 
     private void Update()
     {
-        Cursor.visible = false;
-        transform.position = new Vector3(MainCamera.ScreenToWorldPoint(Input.mousePosition).x, MainCamera.ScreenToWorldPoint(Input.mousePosition).y, 0f);
-        switch(PlayerGeneral.CurrentWeaponReference.Category)
+        if (temp > 10)
         {
-            case 0:
-                GetComponentInChildren<SpriteRenderer>().sprite = CursorSpriteList[1];
-                break;
-            case 1:
-                GetComponentInChildren<SpriteRenderer>().sprite = CursorSpriteList[2];
-                break;
-            case 2:
-                GetComponentInChildren<SpriteRenderer>().sprite = CursorSpriteList[3];
-                break;
-            default:
-                GetComponentInChildren<SpriteRenderer>().sprite = CursorSpriteList[0];
-                break;
+            Cursor.visible = false;
+            transform.position = new Vector3(MainCamera.ScreenToWorldPoint(Input.mousePosition).x, MainCamera.ScreenToWorldPoint(Input.mousePosition).y, 0f);
+            switch (PlayerGeneral.CurrentWeaponReference.Category)
+            {
+                case 0:
+                    GetComponentInChildren<SpriteRenderer>().sprite = CursorSpriteList[1];
+                    break;
+                case 1:
+                    GetComponentInChildren<SpriteRenderer>().sprite = CursorSpriteList[2];
+                    break;
+                case 2:
+                    GetComponentInChildren<SpriteRenderer>().sprite = CursorSpriteList[3];
+                    break;
+                default:
+                    GetComponentInChildren<SpriteRenderer>().sprite = CursorSpriteList[0];
+                    break;
+            }
         }
+        else
+            temp++;
     }
 
     private void Start()
