818aaddb780edcd0aebaeca4cca4e29664fe4636
asian_nerd <ironryan2807@gmail.com>
2020-10-22 02:49:45 +0800

    Script for new PlayerEyes GameObject to follow the mouse position

---
+++ PlayerEyesFollowScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEyesFollowScript : MonoBehaviour
+{
+    public float EyeDistance = 0.1f;
+    void Update()
+    {
+        transform.position = new Vector2(PlayerGeneral.PlayerPosition.x + (Mathf.Cos(PlayerGeneral.MouseAngle) * EyeDistance), PlayerGeneral.PlayerPosition.y + (Mathf.Sin(PlayerGeneral.MouseAngle) * EyeDistance));
+    }
+}
