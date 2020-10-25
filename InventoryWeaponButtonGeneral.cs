3ade60d289565b40c2aaadc50ed061716f641113
asian_nerd <ironryan2807@gmail.com>
2020-10-26 02:28:34 +0800

    InventoryObject(W) Prefab support

--- InventoryWeaponButtonGeneral.cs
+++ InventoryWeaponButtonGeneral.cs
@@ -7,18 +7,44 @@ using UnityEngine.UI;
 public class InventoryWeaponButtonGeneral : MonoBehaviour
 {
     public GameObject Player;
-    public GameObject Parent;
+    public GameObject WeaponSelect;
     public WeaponData.Weapon Weapon;
+    public GameObject Runtime;
+
+    public Button SButton;
+    public GameObject SText;
+    public GameObject SWeaponSprite;
+
+
 
     public void SetEquippedWeapon()
     {
-        Player = GameObject.FindGameObjectWithTag("Player");
         Player.GetComponent<PlayerGeneral>().CurrentWeapon = Weapon;
-        Player.GetComponent<PlayerGeneral>().LastButton = gameObject;
     }
 
     public void SetWeapon(WeaponData.Weapon SelectedWeapon)
     {
         Weapon = SelectedWeapon;
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Runtime = GameObject.FindGameObjectWithTag("RuntimeScript");
+
+        SText.GetComponent<Text>().text = Weapon.Level.ToString();
+
+        
+        switch (Weapon.Category)
+        {
+            case 1:
+                SWeaponSprite.GetComponent<Image>().sprite = Runtime.GetComponent<WeaponData>().RangeWeaponSpriteList[Weapon.WeaponID];
+                SWeaponSprite.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 45));
+                break;
+            case 2:
+                SWeaponSprite.GetComponent<Image>().sprite = Runtime.GetComponent<WeaponData>().ProjectileSpriteList[Weapon.ProjectileSpriteID];
+                SWeaponSprite.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 45));
+                break;
+            default:
+                SWeaponSprite.GetComponent<Image>().sprite = Runtime.GetComponent<WeaponData>().MeleeWeaponSpriteList[Weapon.WeaponID];
+                break;
+        }
+        SWeaponSprite.GetComponent<Image>().SetNativeSize();
     }
 }
