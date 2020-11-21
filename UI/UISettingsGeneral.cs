9b11e81c87c14aa82307310ad9e1b0885972a36b
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2020-11-21 23:17:02 +0800

    Added new Settings inventory

---
+++ UI/UISettingsGeneral.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISettingsGeneral : MonoBehaviour
+{
+    public Slider FPSSlider;
+    public float FPS;
+
+    private void Update()
+    {
+        FPS = FPSSlider.value;
+        FPSSet();
+        Debug.Log(FPS);
+    }
+
+    void FPSSet()
+    {
+        if (FPS < 10)
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 10;
+        }
+        if (FPS == 0)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        if(!(FPS < 10) && !(FPS == 0))
+        {
+            Application.targetFrameRate = Convert.ToInt32(FPS);
+        }
+
+
+    }
+}
